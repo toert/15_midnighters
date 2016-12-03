9c52c76f6b338e30f76e7eea611a5a63aac1dace
toert <pw177@mail.ru>
2016-12-03 10:21:36 +0300

    getting info from api

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,36 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import pytz
+import datetime
 
-def get_midnighters():
+URL = 'https://devman.org/api/challenges/solution_attempts/?page='
+
+
+def request_API_page(number_of_page):
+    URL_of_page = '{}{}'.format(URL, str(number_of_page))
+    return requests.get(URL_of_page)
+
+
+def get_count_of_pages():
+    for page in range(1,100):
+        request = request_API_page(page)
+        if request.status_code == 404:
+            return page-1
+
+
+def load_attempts(pages):
+    for page in range(1, pages):
+        response = request_API_page(page).json()
+        #print(json.dumps(response, indent=4, sort_keys=True, ensure_ascii=False))
+        for record in response['records']:
+            yield {
+                'username': record['username'],
+                'timestamp': record['timestamp'],
+                'timezone': record['timezone'],
+            }
+
+def get_midnighters(records):
     pass
 
 if __name__ == '__main__':
-  pass
+    pages_count = get_count_of_pages()
+    records = list(load_attempts(pages_count))
