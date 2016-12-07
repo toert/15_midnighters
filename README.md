03b1195dc90943ac05e4af8506689790145ff589
toert <pw177@mail.ru>
2016-12-07 21:33:00 +0300

    first initial

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# 15_midnighters
+#Midnighters API Devman
+##What does it do?
+The script finds all users of devman.org which handover their works after midnight.
+##How can I use it?
+You should compile it by Python 3.5:
+	python3.5 seek_dev_nighters.py
+
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,8 +1,12 @@
 import requests
-import pytz
-import datetime
+from pytz import timezone
+from datetime import datetime
+
 
 URL = 'https://devman.org/api/challenges/solution_attempts/?page='
+START_NIGHT = 0
+END_NIGHT = 6
+FIRST_PAGE_NUMBER = 1
 
 
 def request_API_page(number_of_page):
@@ -11,16 +15,13 @@ def request_API_page(number_of_page):
 
 
 def get_count_of_pages():
-    for page in range(1,100):
-        request = request_API_page(page)
-        if request.status_code == 404:
-            return page-1
+    request = request_API_page(FIRST_PAGE_NUMBER).json()
+    return request['number_of_pages']
 
 
 def load_attempts(pages):
-    for page in range(1, pages):
+    for page in range(FIRST_PAGE_NUMBER, pages):
         response = request_API_page(page).json()
-        #print(json.dumps(response, indent=4, sort_keys=True, ensure_ascii=False))
         for record in response['records']:
             yield {
                 'username': record['username'],
@@ -28,9 +29,27 @@ def load_attempts(pages):
                 'timezone': record['timezone'],
             }
 
+
 def get_midnighters(records):
-    pass
+    midnighters = []
+    for record in records:
+        time = datetime.fromtimestamp(record['timestamp'],
+                                      timezone(record['timezone']))
+        hour = datetime.timetuple(time)[3]
+        if hour in range(START_NIGHT, END_NIGHT) and\
+                        record['username'] not in midnighters:
+            midnighters.append(record['username'])
+    return midnighters
+
+
+def print_midnighters(midnighters):
+    print('\"Don\'t want to sleep after 24:00 PM\" guys are:')
+    for sleepman in midnighters:
+        print(sleepman)
+
 
 if __name__ == '__main__':
     pages_count = get_count_of_pages()
     records = list(load_attempts(pages_count))
+    records = list(filter(lambda record: record['timestamp'] is not None, records))
+    print_midnighters(get_midnighters(records))
